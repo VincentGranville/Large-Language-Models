11707bad50418ee4fbd1ea77a2b94142f8cb06f1
Vincent Granville <vincentg@mltechniques.com>
2023-12-17 14:32:59 -0800

    Wolfram: get directory structure, list of URLs and content

--- crawl_directory.py
+++ crawl_directory.py
@@ -10,17 +10,18 @@ URL_list = []
 URL_parent_Category = {}
 categoryLevel = {}
 history = {}
+final_URL = {}
 
 URL_base1 = "https://mathworld.wolfram.com/topics/"  # for directory pages (root)
 URL_base2 = "https://mathworld.wolfram.com/"         # for final pages
 
-# seed_URL = "https://mathworld.wolfram.com/topics/ProbabilityandStatistics.html"
-# seed_category = "Probability and Statistics"  # "Root" if starting at URL_base1
-# categoryLevel[seed_category] = 1  # set to 0 if starting at URL_base1
+seed_URL = "https://mathworld.wolfram.com/topics/ProbabilityandStatistics.html"
+seed_category = "Probability and Statistics"  # "Root" if starting at URL_base1
+categoryLevel[seed_category] = 1  # set to 0 if starting at URL_base1
 
-seed_URL = "https://mathworld.wolfram.com/topics/"
-seed_category = "Root"  # "Root" if starting at URL_base1
-categoryLevel[seed_category] = 0  # set to 0 if starting at URL_base1
+# seed_URL = "https://mathworld.wolfram.com/topics/"
+# seed_category = "Root"  # "Root" if starting at URL_base1
+# categoryLevel[seed_category] = 0  # set to 0 if starting at URL_base1
 
 URL_list.append(seed_URL)   # URL stack
 URL_parent_Category[seed_URL] = seed_category 
@@ -43,6 +44,8 @@ def update_lists(new_URL, new_category, parent_category, file):
     file.write(level+"\t"+new_category+"\t"+parent_category+"\n")
     file.flush()
     return()
+
+#--- Creating category structure and list of webpages
 
 file1 = open("crawl_log.txt","w",encoding="utf-8")
 file2 = open("crawl_categories.txt","w",encoding="utf-8")
@@ -83,7 +86,7 @@ while parsed < min(max_URLs, n_URLs):
             page2 = page.split("<a href=\"/")
             n_URLs_old = n_URLs
 
-            # scraping for Type-1 page
+            # scraping Type-1 page (intermediate directory node) 
 
             for line in page1:  
                 line = line.split("<span>")
@@ -99,7 +102,7 @@ while parsed < min(max_URLs, n_URLs):
                         file1.flush()
                         n_URLs += 1
 
-            # scraping for Type-2 page
+            # scraping Type-2 page (final directory node)
 
             if n_URLs == n_URLs_old:
 
@@ -112,6 +115,30 @@ while parsed < min(max_URLs, n_URLs):
                         update_lists(new_URL, new_category, parent_category, file2)
                         file1.write(new_URL+"\tEndNode\t"+new_category+"\t"+str(level+1)+"\n")
                         file1.flush()
+                        final_URL[new_URL] = (new_category, parent_category, level+1)
 
 file1.close()
 file2.close()
+print()
+
+#--- Extracting content from final URLs
+
+n = len(final_URL)
+count = 0
+file = open("crawl_final.txt","w",encoding="utf-8")
+separator = "\t~"
+
+for URL in final_URL:
+
+    count += 1
+    print("Page: count %d/%d %s" %(count, n, URL))
+    resp = requests.get(URL, timeout=5)
+
+    if resp.status_code == 200:
+        category = str(final_URL[URL])
+        page = resp.text
+        page = page.replace('\n', ' ')
+        file.write(URL+"\t"+category+separator+page+"\n")
+        file.flush()
+
+file.close()
