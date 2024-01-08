b21134992a0435b09b1addd34d8bd1d0e73a0486
Vincent Granville <vincentg@mltechniques.com>
2024-01-08 10:11:33 -0800

    Add files via upload

--- llm5/llm5_short_read.py
+++ llm5/llm5_short_read.py
@@ -5,47 +5,16 @@ import requests
 
 pwd = "https://raw.githubusercontent.com/VincentGranville/Large-Language-Models/main/llm5/"
 
-# words can not be any of these words
-stopwords = ( "of", "now", "have", "so", "since", "but", "and", 
-              "thus", "therefore", "a", "as", "it", "then", "that",
-              "with", "to", "is", "will", "the", "if", "there", "then,",
-              "such", "or", "for", "be", "where", "on", "at", "in", "can",
-              "we", "on", "this", "let", "an", "are", "has", "how", "do",
-              "each", "which", "nor", "any", "all", "al.", "by", "having",
-              "therefore", "another", "having", "some", "obtaining",
-              "into", "does", "union", "few", "makes", "occurs", "were",
-              "here", "these", "after", "defined", "takes", "therefore,",
-              "here,", "note", "more", "considered", "giving", "obtaining" 
-              "etc.", "i.e.,", "Similarly,", "its", "from", "much", "was",
-              "given", "Now,", "instead", "above,", "rather", "consider",
-              "found", "according", "taking", "proved", "now,", "define",
-              "showed", "they", "show", "also", "both", "must", "about",
-              "letting", "gives", "their", "otherwise", "called", "descibed",
-              "related", "content", "eg", "needed", "picks", "yielding",
-              "obtained", "exceed", "until", "complicated", "resulting",
-              "give", "write", "directly", "good", "simply", "direction",
-              "when", "itself", "ie", "al", "usually", "whose", "being",
-              "so-called", "while", "made", "allows", "them", "would", "keeping",
-              "denote", "implemented", "his", "shows", "chosen", "just",
-              "describes", "way", "stated", "follows", "approaches", "known"
-              "result", "sometimes", "corresponds", "every", "referred",
-              "produced", "than", "may", "not", "exactly", "&nbsp;", "whether",
-              "illustration", ",", ".", "...", "states", "says", "known", "exists",
-              "expresses", "respect", "commonly", "describe", "determine", "refer",
-              "often", "relies", "used", "especially", "interesting", "versus",
-              "consists", "arises", "requires", "apply", "assuming", "said"
-            )
-
 # map below to deal with some accented / non-standard characters
-utf_map = { "&nbsp;"   : " ", 
-            "&oacute;" : "e",
-            "&eacute;" : "e",
-            "&ouml;"   : "o",
-            "&ocirc;"  : "o",
-            "&#233;"   : "e",
-            "  "       : " ",
-            "'s"       : "",   # example: Feller's --> Feller
-          }
+#utf_map = { "&nbsp;"   : " ", 
+#            "&oacute;" : "e",
+#            "&eacute;" : "e",
+#            "&ouml;"   : "o",
+#            "&ocirc;"  : "o",
+#            "&#233;"   : "e",
+#            "  "       : " ",
+#            "'s"       : "",   # example: Feller's --> Feller
+#          }
 
 
 #--- some util functions
@@ -149,6 +118,12 @@ def read_word_list(filename, path = pwd):
         if len(line) > 1:
             word_list[line[0]] = text_to_list(line[1])
     return(word_list)
+
+
+def read_stopwords(filename, path = pwd):
+    data = get_data(filename, path)
+    stopwords = text_to_list(data[0])
+    return(stopwords)
 
 
 def read_dictionary(filename, path = pwd):
